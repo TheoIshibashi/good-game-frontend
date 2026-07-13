803fdd96ef9bb4bef2f4bac45e03a20e07bb2a9a
Theo Ishibashi <theoshiba2005@gmail.com>
2026-07-13 15:18:14 -0300

    feat: initialize react state hooks for game search

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import './App.css'
 
+
+interface GameData {
+  title: string;
+  usd_price: number | null;
+  image: string | null;
+}
+
 function App() {
+  const [searchGame, setSearchGame] = useState<string>("");
+  const [game, setGame] = useState<GameData | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const [error, setError] = useState<string | null>(null);
+
   return (
-    <div className="App">
+    <div>
       <h1>Gaming HUB</h1>
     </div>
   )
 }
 
 export default App
-
-
